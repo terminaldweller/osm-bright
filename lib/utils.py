22b89d7df6ea1221ebf87e23ea0a09151a4b99ac
bloodstalker <thabogre@gmail.com>
2020-12-22 09:01:03 +0330

    python2...

--- lib/utils.py
+++ lib/utils.py
@@ -2,6 +2,7 @@ import os
 from distutils.file_util import copy_file, DistutilsFileError
 from distutils.dir_util import mkpath
 
+
 def copy_tree(src, dst, ignores=()):
     """Copy an entire directory tree 'src' to a new location 'dst'.
 
@@ -11,7 +12,7 @@ def copy_tree(src, dst, ignores=()):
     file in 'src' is copied to 'dst', and directories under 'src' are
     recursively copied to 'dst'.  Return the list of files that were
     copied or might have been copied, using their output name.
-    
+
     Ignore any file whose name is in the "ignores" iterable.
 
     This is a forked version of distutils.dir_util.copy_tree, which
@@ -22,20 +23,22 @@ def copy_tree(src, dst, ignores=()):
 
     try:
         names = os.listdir(src)
-    except os.error, (errno, errstr):
-        raise DistutilsFileError, "error listing files in '%s': %s" % (src, errstr)
+    except os.error, (os.errno, errstr):
+        raise DistutilsFileError,
+        "error listing files in '%s': %s" % (src, errstr)
 
     mkpath(dst)
 
     outputs = []
 
     for n in names:
-        if n in ignores: continue
+        if n in ignores:
+            continue
 
         src_name = os.path.join(src, n)
         dst_name = os.path.join(dst, n)
 
-#def copy_tree(src, dst, preserve_mode=1, preserve_times=1,
+# def copy_tree(src, dst, preserve_mode=1, preserve_times=1,
 #              preserve_symlinks=0, update=0, verbose=1, dry_run=0):
 
         if os.path.islink(src_name):
